392f807c5451abffe5a70a5f870b44a7b679e5ad
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-10-19 17:51:32 +0100

    git ignore, make installable package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = "stencil",
+      version = "1.0.0",
+      description = "Plot Tools for PP theses",
+      author = "Jack Dunger",
+      packages = ["stencil"]
+)
